489ea90fd2eb1c918b8b12cebbe28054c7cf7157
Fu Yin <fooying@qq.com>
2021-07-15 00:50:28 +0800

    patch is not support buy weapp

--- src/fetch.tsx
+++ src/fetch.tsx
@@ -59,7 +59,7 @@ function fetch({
 }: {
   url: string;
   data?: object | string;
-  method?: keyof Taro.request.method | undefined;
+  method?: keyof Taro.request.method;
 }): any {
   Taro.showLoading({
     title: '加载中...',
